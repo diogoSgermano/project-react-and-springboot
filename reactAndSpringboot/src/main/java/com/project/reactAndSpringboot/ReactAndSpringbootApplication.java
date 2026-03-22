428c61a505250e430645887a7a243eae90cdc76e
Diogo Soares Germano <diogogermano28@gmail.com>
2026-03-22 16:44:36 -0300

    Create project SpringBoot

---
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/ReactAndSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.project.reactAndSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringbootApplication.class, args);
+	}
+
+}
---
+++ reactAndSpringboot/src/test/java/com/project/reactAndSpringboot/ReactAndSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reactAndSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
