38afecd5629d9f6f9fe7d90198b2f5e44b9bb543
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-19 17:13:53 -0300

    refactor(product): Simplify architecture for a basic design.

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/dto/ProdutoDTO.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/dto/ProdutoDTO.java
@@ -4,7 +4,8 @@ import com.project.reactAndSpringboot.entity.Produto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record ProdutoRequestDTO(
+public record ProdutoDTO(
+        Long codigo,
 
         @NotBlank(message = "Nome é obrigatório")
         @Size(max = 100, message = "Nome deve ter no máximo 100 caracteres")
@@ -14,9 +15,13 @@ public record ProdutoRequestDTO(
         @Size(max = 100, message = "Marca deve ter no máximo 100 caracteres")
         String marca
 ) {
+    public ProdutoDTO(Produto produto) {
+        this(produto.getCodigo(), produto.getNome(), produto.getMarca());
+    }
 
     public Produto toEntity() {
         Produto produto = new Produto();
+        produto.setCodigo(this.codigo);
         produto.setNome(this.nome);
         produto.setMarca(this.marca);
         return produto;
