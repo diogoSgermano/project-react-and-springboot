9a699de07090728c9bc0519b1025b1493e453e13
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-19 17:16:04 -0300

    feat(main): add project link in terminal

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/ReactAndSpringbootApplication.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/ReactAndSpringbootApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReactAndSpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactAndSpringbootApplication.class, args);
-	}
 
+        SpringApplication.run(ReactAndSpringbootApplication.class, args);
+        System.out.println("Servidor rodando em: http://localhost:8080/");
+	}
 }
