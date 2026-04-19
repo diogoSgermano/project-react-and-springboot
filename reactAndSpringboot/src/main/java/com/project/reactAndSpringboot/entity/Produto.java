f331c8af62e42fd6f295fc19bed59025d89eedf9
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-19 17:14:57 -0300

    chore(product): Remove getter from lombok

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/entity/Produto.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/entity/Produto.java
@@ -2,7 +2,7 @@ package com.project.reactAndSpringboot.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
+
 
 @Entity
 @Table (name="produto")
