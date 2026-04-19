110ba5f1f2f084c6a5129c8d53233001125de769
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-19 17:11:36 -0300

    chore(product): create ProdutoController
    add API in name

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/controller/ProdutoController.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/controller/ProdutoController.java
@@ -1,7 +1,12 @@
 package com.project.reactAndSpringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProdutoController {
+    @GetMapping("/")
+    public String teste(){
+        return "Hello, world!";
+    }
 }
