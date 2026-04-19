77a44664cf4393174464d7e0929881ef84061f16
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-19 17:16:49 -0300

    chore(product): create Respository

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/repository/ProdutoRepository.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/repository/ProdutoRepository.java
@@ -2,6 +2,9 @@ package com.project.reactAndSpringboot.repository;
 
 import com.project.reactAndSpringboot.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
 }
