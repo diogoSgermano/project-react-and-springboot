89263d8466bacb94c0f9c6575f058e78593db384
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-21 11:54:52 -0300

    feat(product): create Produto
     and add function findAll

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/service/ProdutoService.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/service/ProdutoService.java
@@ -1,4 +1,22 @@
 package com.project.reactAndSpringboot.service;
 
+import com.project.reactAndSpringboot.entity.Produto;
+import com.project.reactAndSpringboot.repository.ProdutoRepository;
+import org.jvnet.hk2.annotations.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> listarTodos(){
+        return produtoRepository.findAll();
+    }
+
 }
