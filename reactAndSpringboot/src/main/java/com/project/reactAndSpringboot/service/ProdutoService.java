ae5393af2483155031b339aef9da3d2d3a3182ae
Diogo Soares Germano <diogogermano28@gmail.com>
2026-04-21 17:25:26 -0300

    feat(product): add registration method

--- reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/service/ProdutoService.java
+++ reactAndSpringboot/src/main/java/com/project/reactAndSpringboot/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package com.project.reactAndSpringboot.service;
 
 import com.project.reactAndSpringboot.dto.ProdutoDTO;
+import com.project.reactAndSpringboot.entity.Produto;
+import com.project.reactAndSpringboot.exception.ProdutoExistsException;
 import com.project.reactAndSpringboot.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +18,24 @@ public class ProdutoService {
         this.produtoRepository = produtoRepository;
     }
 
+
     public List<ProdutoDTO> listarTodos() {
         return produtoRepository.findAll()
                 .stream()
                 .map(ProdutoDTO::new)
                 .collect(Collectors.toList());
     }
+
+    public ProdutoDTO cadastrar(ProdutoDTO dto){
+        if(produtoRepository.existsByNome(dto.nome())){
+            throw new ProdutoExistsException(dto.nome());
+        }
+        Produto produto = new Produto();
+        produto.setNome(dto.nome());
+        produto.setMarca(dto.marca());
+        produto = produtoRepository.save(produto);
+        return new ProdutoDTO(produto);
+
+    }
+
 }
